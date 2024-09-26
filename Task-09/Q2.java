90fa1a786ccd612aa168200acd799606cb316e2a
Rajapandi2559 <raju.rs2507@gmail.com>
2024-09-26 21:26:59 +0530

    Add files via upload

---
+++ Task-09/Q2.java
@@ -0,0 +1,23 @@
+package task_9;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Q2 {
+
+	public static void main(String[] args) {
+		
+		WebDriver driver=new ChromeDriver();     // Creating an object of FirefoxDriver
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		driver.navigate().to("https://www.demoblaze.com/");
+		driver.manage().window().maximize();    // Maximize the window
+		if (driver.getTitle().equals("STORE")) {
+			System.out.println("Page landed on correct website");
+		} else {
+			System.out.println("Page not landed on correct website");
+		}
+		driver.close();                         // Closing the browser automatically
+	}
+}
